940bdcf2f14ca6fa82dc85813887e5933eadb479
Evgeniy <evgeniy.sologub.vl@gmail.com>
2023-06-24 09:19:35 +1000

    upload

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import store from "./store/createStore";
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
+
 root.render(
     <Provider store={store}>
         <BrowserRouter>
